2bc5c3af79b315527283870e29f1f33f14068c84
Multinite <founders.multinite@gmail.com>
2024-08-09 09:23:51 +1000

    Add: Tiptap Editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import Logo from "@/../public/logo_nobg.png";
+import Logo from "@/../public/logo.webp";
 import MainNav from "@/components/navbar/Navbar";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
@@ -14,96 +14,95 @@ export default function Home() {
     <div className="w-full h-[calc(100vh-64px)] overflow-hidden">
       <MainNav />
       <main className="flex justify-center items-center flex-col w-full h-full">
-        <Image src={Logo} width={200} height={200} alt="logo" />
+        <Image src={Logo} width={200} height={200} alt="logo" className="rounded-large" />
         <div className="w-full h-full flex justify-center items-center">
-          <SpeedTestText />
         </div>
       </main>
     </div>
   );
 }
-//@ts-ignore
-const fetcher = (url) => fetch(url).then((res) => res.text());
+// //@ts-ignore
+// const fetcher = (url) => fetch(url).then((res) => res.text());
 
-function SpeedTestText() {
-  const { data, error, isLoading } = useSWR("/api/chat", fetcher);
+// function SpeedTestText() {
+//   const { data, error, isLoading } = useSWR("/api/chat", fetcher);
 
-  const [currentCharIndex, $currentCharIndex] = useState(0);
-  const [text, $text] = useState("");
-  const [ref, bounds] = useMeasure();
+//   const [currentCharIndex, $currentCharIndex] = useState(0);
+//   const [text, $text] = useState("");
+//   const [ref, bounds] = useMeasure();
 
-  return (
-    <div className="speed-test flex flex-col mt-32 w-[80vw] h-full relative text-3xl leading-10">
-      {isLoading ? (
-        <Spinner size="lg" color="primary" label="Loading..." />
-      ) : (
-        <>
-          <motion.div
-            className="absolute inset-0 text-foreground pointer-events-none select-non flex w-fit"
-            draggable={false}
-            animate={{ x: window.innerWidth / 2 - bounds.width - bounds.right }}
-            ref={ref}
-          >
-            {text.split("").map((char, index) => (
-              <motion.span
-                key={index}
-                className={cn(
-                  data?.split("")[index] === char ? "" : "text-danger",
-                  "block min-w-[18.1px]"
-                )}
-                initial={{ x: -10, opacity: 1 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ type: "just", duration: 0.05 }}
-              >
-                {char}
-              </motion.span>
-            ))}
-          </motion.div>
-          <motion.div
-            contentEditable
-            className="absolute inset-0 outline-none h-full text-transparent flex w-fit"
-            spellCheck="false"
-            suppressContentEditableWarning
-            // animate={{ x: window.innerWidth / 2 - bounds.width - bounds.right }}
-            onInput={(e) => {
-              e.preventDefault();
-              const txt = e.currentTarget.innerText;
-              $text(txt);
-              $currentCharIndex(txt.length);
-            }}
-          ></motion.div>
-          <motion.div
-            className="flex"
-            animate={{ x: window.innerWidth / 2 - bounds.width - bounds.right }}
+//   return (
+//     <div className="speed-test flex flex-col mt-32 w-[80vw] h-full relative text-3xl leading-10">
+//       {isLoading ? (
+//         <Spinner size="lg" color="primary" label="Loading..." />
+//       ) : (
+//         <>
+//           <motion.div
+//             className="absolute inset-0 text-foreground pointer-events-none select-non flex w-fit"
+//             draggable={false}
+//             animate={{ x: window.innerWidth / 2 - bounds.width - bounds.right }}
+//             ref={ref}
+//           >
+//             {text.split("").map((char, index) => (
+//               <motion.span
+//                 key={index} 
+//                 className={cn(
+//                   data?.split("")[index] === char ? "" : "text-danger",
+//                   "block min-w-[18.1px]"
+//                 )}
+//                 initial={{ x: -10, opacity: 1 }}
+//                 animate={{ x: 0, opacity: 1 }}
+//                 transition={{ type: "just", duration: 0.05 }}
+//               >
+//                 {char}
+//               </motion.span>
+//             ))}
+//           </motion.div>
+//           <motion.div
+//             contentEditable
+//             className="absolute inset-0 outline-none h-full text-transparent flex w-fit"
+//             spellCheck="false"
+//             suppressContentEditableWarning
+//             // animate={{ x: window.innerWidth / 2 - bounds.width - bounds.right }}
+//             onInput={(e) => {
+//               e.preventDefault();
+//               const txt = e.currentTarget.innerText;
+//               $text(txt);
+//               $currentCharIndex(txt.length);
+//             }}
+//           ></motion.div>
+//           <motion.div
+//             className="flex"
+//             animate={{ x: window.innerWidth / 2 - bounds.width - bounds.right }}
 
-            // animate={{ x: -18.1 * currentCharIndex }}
-          >
-            {data?.split("").map((char, index) => {
-              const userEnteredChar = text?.split("")[index];
-              return (
-                <span
-                  key={index}
-                  className={cn(
-                    currentCharIndex > index
-                      ? "text-transparent"
-                      : "text-foreground-500",
-                    userEnteredChar !== char && userEnteredChar != undefined
-                      ? "text-transparent"
-                      : "",
-                    "min-w-[18.1px] block"
-                  )}
-                >
-                  {userEnteredChar === char && userEnteredChar != undefined
-                    ? char
-                    : userEnteredChar != undefined
-                    ? userEnteredChar
-                    : char}
-                </span>
-              );
-            })}
-          </motion.div>
-        </>
-      )}
-    </div>
-  );
-}
+//             // animate={{ x: -18.1 * currentCharIndex }}
+//           >
+//             {data?.split("").map((char, index) => {
+//               const userEnteredChar = text?.split("")[index];
+//               return (
+//                 <span
+//                   key={index}
+//                   className={cn(
+//                     currentCharIndex > index
+//                       ? "text-transparent"
+//                       : "text-foreground-500",
+//                     userEnteredChar !== char && userEnteredChar != undefined
+//                       ? "text-transparent"
+//                       : "",
+//                     "min-w-[18.1px] block"
+//                   )}
+//                 >
+//                   {userEnteredChar === char && userEnteredChar != undefined
+//                     ? char
+//                     : userEnteredChar != undefined
+//                     ? userEnteredChar
+//                     : char}
+//                 </span>
+//               );
+//             })}
+//           </motion.div>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
---
+++ src/hooks/useBlockEditor.ts
@@ -0,0 +1,133 @@
+import { useContext, useEffect, useMemo, useState } from 'react'
+
+import { Editor, useEditor } from '@tiptap/react'
+import Ai from '@tiptap-pro/extension-ai'
+import Collaboration from '@tiptap/extension-collaboration'
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
+import { TiptapCollabProvider, WebSocketStatus } from '@hocuspocus/provider'
+import type { Doc as YDoc } from 'yjs'
+
+import { ExtensionKit } from '@/extensions/extension-kit'
+import { EditorContext } from '../context/EditorContext'
+import { userColors, userNames } from '../lib/constants'
+import { randomElement } from '../lib/utils'
+import { EditorUser } from '../components/BlockEditor/types'
+import { useSidebar } from './useSidebar'
+import { initialContent } from '@/lib/data/initialContent'
+
+const TIPTAP_AI_APP_ID = process.env.NEXT_PUBLIC_TIPTAP_AI_APP_ID
+const TIPTAP_AI_BASE_URL = process.env.NEXT_PUBLIC_TIPTAP_AI_BASE_URL || 'https://api.tiptap.dev/v1/ai'
+
+declare global {
+  interface Window {
+    editor: Editor | null
+  }
+}
+
+export const useBlockEditor = ({
+  aiToken,
+  ydoc,
+  provider,
+}: {
+  aiToken: string
+  ydoc: YDoc
+  provider?: TiptapCollabProvider | null | undefined
+}) => {
+  const leftSidebar = useSidebar()
+  const [collabState, setCollabState] = useState<WebSocketStatus>(
+    provider ? WebSocketStatus.Connecting : WebSocketStatus.Disconnected,
+  )
+  const { setIsAiLoading, setAiError } = useContext(EditorContext)
+
+  const editor = useEditor(
+    {
+      immediatelyRender: false,
+      autofocus: true,
+      onCreate: ({ editor }) => {
+        if (provider) {
+          provider.on('synced', () => {
+            if (editor.isEmpty) {
+              editor.commands.setContent(initialContent)
+            }
+          })
+        } else {
+          editor.commands.setContent(initialContent)
+          editor.commands.focus('start', { scrollIntoView: true })
+        }
+      },
+      extensions: [
+        ...ExtensionKit({
+          provider,
+        }),
+        provider
+          ? Collaboration.configure({
+              document: ydoc,
+            })
+          : undefined,
+        provider
+          ? CollaborationCursor.configure({
+              provider,
+              user: {
+                name: randomElement(userNames),
+                color: randomElement(userColors),
+              },
+            })
+          : undefined,
+        Ai.configure({
+          appId: TIPTAP_AI_APP_ID,
+          token: aiToken,
+          baseUrl: TIPTAP_AI_BASE_URL,
+          autocompletion: true,
+          onLoading: () => {
+            setIsAiLoading(true)
+            setAiError(null)
+          },
+          onSuccess: () => {
+            setIsAiLoading(false)
+            setAiError(null)
+          },
+          onError: error => {
+            setIsAiLoading(false)
+            setAiError(error.message)
+          },
+        }),
+      ].filter(e => !!e),
+      editorProps: {
+        attributes: {
+          autocomplete: 'off',
+          autocorrect: 'off',
+          autocapitalize: 'off',
+          class: 'min-h-full',
+        },
+      },
+    },
+    [ydoc, provider],
+  )
+
+  const users = useMemo(() => {
+    if (!editor?.storage.collaborationCursor?.users) {
+      return []
+    }
+
+    return editor.storage.collaborationCursor?.users.map((user: EditorUser) => {
+      const names = user.name?.split(' ')
+      const firstName = names?.[0]
+      const lastName = names?.[names.length - 1]
+      const initials = `${firstName?.[0] || '?'}${lastName?.[0] || '?'}`
+
+      return { ...user, initials: initials.length ? initials : '?' }
+    })
+  }, [editor?.storage.collaborationCursor?.users])
+
+  const characterCount = editor?.storage.characterCount || { characters: () => 0, words: () => 0 }
+
+  useEffect(() => {
+    provider?.on('status', (event: { status: WebSocketStatus }) => {
+      setCollabState(event.status)
+    })
+  }, [provider])
+
+  window.editor = editor
+
+  return { editor, users, characterCount, collabState, leftSidebar }
+}
